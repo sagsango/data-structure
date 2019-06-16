a8ee0b1008f15353e3498187a9133a6bf95c1c1d
sagsango <34793802+sagsango@users.noreply.github.com>
2019-06-16 22:46:11 +0530

    Update Insertion Sort  .cpp

--- Sorting/Insertion Sort  .cpp
+++ Sorting/Insertion Sort  .cpp
@@ -2,8 +2,13 @@ void insertion_sort(int arr[],int n)
 { 
     for(int i=0;i<n;i++)
     {
-        int j=i;
-        while( j-1 >= 0  && arr[j-1] > arr[i] )j--;
-        swap(arr[j],arr[i]);
+        int j=i,I=i;
+        while( j-1 >= 0  && arr[j-1] > arr[I] )
+        {
+            j--;
+            swap(arr[j],arr[I]);
+            I=j;
+        }
+            
     }
 }
