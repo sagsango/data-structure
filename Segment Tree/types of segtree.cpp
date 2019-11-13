e0f7b970ca813f429e70e4110792d040c0b81c41
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-14 03:16:05 +0530

    Update types of segtree.cpp

--- Segment Tree/types of segtree.cpp
+++ Segment Tree/types of segtree.cpp
@@ -1,7 +1,7 @@
 Problem           :   https://codeforces.com/gym/259774/problem/A
 Implimentation    :   https://codeforces.com/gym/259774/submission/64850011
 
-1.point updation and point query : No need of segtree
+1.point updation and point query : No need of any tree, array is enough
 2.point updation and range query : segtree only
 3.range updation and point query : only lazy tree 
 4.range updation and range query : segtree and lazy tree both 
