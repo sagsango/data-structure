7ef17f8dfb3481a721621d9036c75a85062860c0
sagsango <34793802+sagsango@users.noreply.github.com>
2019-11-15 02:37:37 +0530

    Create Blog and discussion .cpp

---
+++ Segment Tree/Blog and discussion .cpp
@@ -0,0 +1,13 @@
+https://codeforces.com/blog/entry/22616
+https://codeforces.com/topic/66587/en1
+https://codeforces.com/blog/entry/57319
+https://codeforces.com/blog/entry/18051
+https://codeforces.com/blog/entry/10493
+https://codeforces.com/blog/entry/15890
+https://codeforces.com/blog/entry/6281
+https://codeforces.com/blog/entry/10722
+
+https://codeforces.com/blog/entry/10521
+https://codeforces.com/contest/380
+https://codeforces.com/contest/383
+
