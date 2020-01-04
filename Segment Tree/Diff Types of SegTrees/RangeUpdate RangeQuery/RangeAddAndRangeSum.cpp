65d652b367aa59c4865d25a9934e9c965ab2d05a
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-01-04 09:05:22 +0530

    Create RangeAddAndRangeSum.cpp

---
+++ Segment Tree/Diff Types of SegTrees/RangeUpdate RangeQuery/RangeAddAndRangeSum.cpp
@@ -0,0 +1,44 @@
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] =  t[2*v] + t[2*v+1];
+    }
+}
+
+void push(int v) {
+    t[v*2] += lazy[v];
+    lazy[v*2] += lazy[v];
+    t[v*2+1] += lazy[v];
+    lazy[v*2+1] += lazy[v];
+    lazy[v] = 0;
+}
+
+void update(int v, int tl, int tr, int l, int r, int addend) {
+    if (l > r) 
+        return;
+    if (l == tl && tr == r) {
+        t[v] += addend;
+        lazy[v] += addend;
+    } else {
+        push(v);
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), addend);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, addend);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+
+int query(int v, int tl, int tr, int l, int r) {
+    if (l > r)
+        return 0;
+    if (l == tl && tr == r)
+        return t[v];
+    push(v);
+    int tm = (tl + tr) / 2;
+    return query(v*2, tl, tm, l, min(r, tm)) +
+               query(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
