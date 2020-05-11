0ee25ca5e826d4e72036c638a471f2489ad05910
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-05-12 01:16:22 +0530

    Create RangeAddRangeMin.cpp

---
+++ Segment Tree/Diff Types of SegTrees/RangeUpdate RangeQuery/RangeAddRangeMin.cpp
@@ -0,0 +1,49 @@
+// Tested  :  https://codeforces.com/gym/279715/submission/79778667
+
+const int maxn = 2e5 + 10;
+vector<int>a(maxn),b(maxn),t(4*maxn),lazy(4*maxn);
+int n;
+void build(int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(v*2, tl, tm);
+        build(v*2+1, tm+1, tr);
+        t[v] = min( t[2*v] , t[2*v+1] );
+    }
+}
+ 
+void push(int v) {
+    t[v*2] += lazy[v];
+    lazy[v*2] += lazy[v];
+    t[v*2+1] += lazy[v];
+    lazy[v*2+1] += lazy[v];
+    lazy[v] = 0;
+}
+ 
+void update(int v, int tl, int tr, int l, int r, int addend) {
+    if (l > r) 
+        return;
+    if (l == tl && tr == r) {
+        t[v] += addend;
+        lazy[v] += addend;
+    } else {
+        push(v);
+        int tm = (tl + tr) / 2;
+        update(v*2, tl, tm, l, min(r, tm), addend);
+        update(v*2+1, tm+1, tr, max(l, tm+1), r, addend);
+        t[v] = min(t[v*2], t[v*2+1]);
+    }
+}
+ 
+int query(int v, int tl, int tr, int l, int r) {
+    if (l > r)
+        return 1e15;
+    if (l <= tl && tr <= r)
+        return t[v];
+    push(v);
+    int tm = (tl + tr) / 2;
+    return min(query(v*2, tl, tm, l, min(r, tm)), 
+               query(v*2+1, tm+1, tr, max(l, tm+1), r));
+}
