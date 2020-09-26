acf8126405925d03396422baafa20b2b4130980a
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-09-26 15:19:16 +0530

    Update RangeAddRangeMax&Min.cpp

--- Segment Tree/Diff Types of SegTrees/RangeUpdate RangeQuery/RangeAddRangeMax&Min.cpp
+++ Segment Tree/Diff Types of SegTrees/RangeUpdate RangeQuery/RangeAddRangeMax&Min.cpp
@@ -1,8 +1,48 @@
 see  : https://codeforces.com/gym/262908/submission/66818759
 
+const int maxn = 1e6;
+//vector<int>tmax(maxn<<2),tmin(maxn<<2),lazy(maxn<<2);
+//vector<char>a(maxn);
+char a[maxn];
+int tmax[maxn<<2];
+int tmin[maxn<<2];
+int lazy[maxn<<2];
+#define tm ((tl+tr)>>1)
+void push(int rt){
+	tmax[rt<<1]+=lazy[rt];
+	tmin[rt<<1]+=lazy[rt];
+	lazy[rt<<1]+=lazy[rt];
+	tmax[rt<<1|1]+=lazy[rt];
+	tmin[rt<<1|1]+=lazy[rt];
+	lazy[rt<<1|1]+=lazy[rt];
+	lazy[rt]=0;
+}
+void update(int rt,int tl,int tr,int l,int r,int addend){
+	if( l > r ) return;
+	if( tl == l && tr == r ){ 
+		tmax[rt]+=addend,tmin[rt]+=addend,lazy[rt]+=addend;
+		return;
+	}
+	push(rt);
+	update(rt<<1,tl,tm,l,min(r,tm),addend);
+	update(rt<<1|1,tm+1,tr,max(l,tm+1),r,addend);
+	tmax[rt]=max(tmax[rt<<1],tmax[rt<<1|1]);
+	tmin[rt]=min(tmin[rt<<1],tmin[rt<<1|1]);
+}
+pair<int,int> query(int rt,int tl,int tr,int l,int r){// max,min
+	if( l > r ) return {-1e9,+1e9};
+	if( tl == l && tr == r ) return {tmax[rt],tmin[rt]};
+	push(rt);
+	pair<int,int> ql = query(rt<<1,tl,tm,l,min(r,tm));
+	pair<int,int> qr = query(rt<<1|1,tm+1,tr,max(tm+1,l),r);
+	return {max(ql.first,qr.first),min(ql.second,qr.second)};
+}
+
+--------------------------------------------------------------------------------------------------------------------------
+    
 const int MAXN=1e6+1;
 const int inf = 1e9;
-int a[MAXN];
+char a[MAXN];
 int tmax[4*MAXN];
 int tmin[4*MAXN];
 int lazy[4*MAXN];
